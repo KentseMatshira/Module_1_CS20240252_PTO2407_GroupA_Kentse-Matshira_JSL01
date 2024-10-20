f12c2754f7d15753d9800b5860046be4bb13ade0
KentseMatshira <kmatshira@yahoo.com>
2024-10-20 14:15:38 +0200

    adding the validation logic

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
+ if (regex.test(input)) {
+   result = "Valid Syntax";
+   document.getElementById("result").className = "valid emoji";
+ } else {
+   result = "Invalid Syntax";
+   document.getElementById("result").className = "invalid emoji";
+ }   
             document.getElementById('result').innerText = result;
 }
 
